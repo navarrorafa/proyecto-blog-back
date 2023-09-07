7a692cc1e4b3e78d30c4b4e183b9b9ebbf2e7241
navarrorafa <115833775+navarrorafa@users.noreply.github.com>
2023-09-07 14:47:29 +0200

    cambio con multer

--- controllers/newController.js
+++ controllers/newController.js
@@ -1,13 +1,70 @@
 /** Require o Schema de noticias para criar en la base de datos */
 const New = require("../models/newsModel");
 const jwt = require('jsonwebtoken');
+const  {uploadImagem} = require('../utils/uploadUtils')
 
 //POST CREAR NOTICIAS
+// const newCreate = async (req, res) => {
+//     try {
+
+//       // Requer o token da cookie
+//       const token =  req.header('x-auth-token');
+  
+//       // Verifica se existe cookie
+//       if (!token) {
+//         return res.status(401).json({
+//           ok: false,
+//           msg: 'Acesso não autorizado',
+//         });
+//       }
+  
+//       // Decodifica o token
+//       const decoded = jwt.verify(token, process.env.SECRET_KEY);
+//       console.log(decoded)
+//       console.log(decoded.role)
+  
+//       // Verifica se o usuário é um editor
+//       if (decoded.role === 'editor') {
+//         const newBlog = new New(req.body);
+//         const { title } = newBlog;
+  
+//         // Verifica se a notícia já existe pelo título
+//         const existe = await New.findOne({ title: title });
+  
+//         if (existe) {
+//           return res.status(400).json({
+//             msg: 'Notícia existente ou publicada',
+//           });
+//         }
+  
+//         // Crie a notícia
+//         const saveNews = await newBlog.save();
+  
+//         return res.status(201).json({
+//           ok: true,
+//           msg: 'Notícia criada com sucesso',
+//           news: saveNews,
+//         });
+//       } else {
+//         // Se o usuário não for um editor, retorne um erro de acesso não autorizado
+//         return res.status(403).json({
+//           ok: false,
+//           msg: 'Apenas editores podem criar notícias',
+//         });
+//       }
+//     } catch (error) {
+//       console.error(error);
+//       return res.status(500).json({
+//         ok: false,
+//         msg: 'Entre em contato com o administrador',
+//       });
+//     }
+//   };
+
 const newCreate = async (req, res) => {
     try {
-
       // Requer o token da cookie
-      const token =  req.header('x-auth-token');
+      const token = req.header('x-auth-token');
   
       // Verifica se existe cookie
       if (!token) {
@@ -19,12 +76,19 @@ const newCreate = async (req, res) => {
   
       // Decodifica o token
       const decoded = jwt.verify(token, process.env.SECRET_KEY);
-      console.log(decoded)
-      console.log(decoded.role)
+      console.log(decoded);
+      console.log(decoded.role);
   
       // Verifica se o usuário é um editor
       if (decoded.role === 'editor') {
-        const newBlog = new New(req.body);
+        // Crie a notícia
+        const newBlog = new New({
+          title: req.body.title,
+          text: req.body.text,
+          resume: req.body.resume,
+          author: req.body.author,
+          image: req.body.image, // Aqui está o nome do arquivo da imagemfile.filename
+        });
         const { title } = newBlog;
   
         // Verifica se a notícia já existe pelo título
@@ -59,6 +123,7 @@ const newCreate = async (req, res) => {
       });
     }
   };
+  
   
 
 //POST UPDATE
